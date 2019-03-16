1e9a1a5ce1a6a5e3c0c35f37419f8612df0144a5
sergiotvidal <sergiotvidal2@gmail.com>
2019-03-16 09:35:59 +0100

    dia10 completed, added dia11

--- dia10/ejercicio3.js
+++ dia10/ejercicio3.js
@@ -3,17 +3,26 @@
 // yy un metodo general de reproduccion que al pasarle dos delfines con el mismo color
 // nos da un true (mini dolphin)
 
+class Animal {
+  constructor(eyes = true) {
+    this.eyes = eyes;
+  }
+
+  respirar() {
+    console.log("estoy respirando");
+  }
+}
 class Dolphin extends Animal {
   constructor(name, weight, long, color) {
-    super();
-    this.name = name;
+    super(2);
+    this._name = name;
     this.weight = weight;
     this.long = long;
     this.color = color;
   }
 
   getName() {
-    return this._name;
+    return this._name; // el barrabaja "simula" que es una variable privada es decir que no puedes hacer d1._name y lo tienes que coger a traves del metodo getName
   }
 
   setName() {}
@@ -29,6 +38,8 @@ class Dolphin extends Animal {
 
 let d1 = new Dolphin("Juanito", 199, 200, "red");
 let d2 = new Dolphin("Juanita", 199, 200, "red");
+
+Dolphin.reproduction(d1, d2);
 
 d1.jump();
 Dolphin.reproduction(d1, d2);
